69e8958b2be1c88cf2252d9e517d46aeed4e66f4
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-11 17:03:32 +0330

    refactored ArticleSerializer

--- article/serializers.py
+++ article/serializers.py
@@ -4,26 +4,26 @@ from django.db.models import Avg
 from .models import Article, Review
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    users_count = serializers.SerializerMethodField()
+class eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MDkwMzYyLCJpYXQiOjE3MDc2NTgzNjIsImp0aSI6ImJmZDNmZjM0ODYxMjQ0YmQ4ZjY1MTQ3ODcyYTQ3NGYxIiwidXNlcl9pZCI6MX0.2DpaKg2A4ZMfvGuahlNfsA7RFrX6V73GeHwbKHJqpEY(serializers.ModelSerializer):
+    users_reviewed_count = serializers.SerializerMethodField()
     review_average = serializers.SerializerMethodField()
     user_point = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
-        fields = ["title", "text", "users_count", "review_average", "user_point"]
+        fields = ["title", "text", "users_reviewed_count", "review_average", "user_point"]
 
-    def get_users_count(self, obj):
+    def get_users_reviewed_count(self, obj) -> int:
         return Review.objects.filter(article=obj).count()
 
-    def get_review_average(self, obj):
+    def get_review_average(self, obj) -> float:
         return (
             Review.objects.filter(article=obj)
             .aggregate(review_average=Avg("point"))
             .get("review_average")
         )
 
-    def get_user_point(self, obj):
+    def get_user_point(self, obj) -> int:
         user = self.context.get("request").user
         review = Review.objects.filter(article=obj, user=user)
         if not review.exists():
